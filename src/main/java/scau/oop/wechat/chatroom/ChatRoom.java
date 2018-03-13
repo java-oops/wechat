d1b0b332bcb3d5ab1ee2786f246435899403e80a
chen zefan <czfshine@outlook.com>
2018-03-13 05:19:36 +0000

    增加Chatroom说明

--- src/main/java/scau/oop/wechat/chatroom/ChatRoom.java
+++ src/main/java/scau/oop/wechat/chatroom/ChatRoom.java
@@ -3,13 +3,15 @@ package scau.oop.wechat.chatroom;
 import java.util.Date;
 
 /**
+ * 聊天室
  * @author:czfshine
  * @date:2018/3/12 14:52
  */
 
 public class ChatRoom {
-    private Person[] persons;
-    private String name;
-    private String avatarPath;
-    private Date time;
+    private Person[] persons; //成员列表
+    private String name; 
+    private String avatarPath; //头像图片路径
+    private Date time; //最后聊天时间
+    private message[] msgs;
 }
